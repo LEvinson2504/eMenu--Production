583feb841cd2c47bbdb6015a1175bf3c5ddf93c2
LEvinson2504 <ldsouza2504@gmail.com>
2020-01-30 21:40:05 +0530

    added db query

--- app.js
+++ app.js
@@ -8,11 +8,27 @@ app.use(express.json({limit: "1mb"}));
 
 const database = new Datastore("database.db");
 database.loadDatabase();
+
+app.get("/api", (req, res) => {
+  database.find({}, (err, data) => {
+    if(err) {
+      res.json({status: err});
+      res.end();
+      return;
+    }
+    res.json(data);
+  });
+  
+});
+
+
+
 app.post("/menu", (req, res) => {
   console.log(req.body);
   console.log("I got a request!");
   const menu = req.body;
   const timeStamp = Date.now();
+  database.remove({}, { multi: true });
   database.insert(menu);
   res.json({
     status: "done!",
